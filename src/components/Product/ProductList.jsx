8c56e9b2e3c75697829bb9c7e2c5f1b77755b0a7
Archana <rathourarchna922@gmail.com>
2026-07-04 02:54:18 +0530

    Display product list using custom hook

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import ProductItem from "../ProductItem/ProductItem";
+import useProducts from "../../hooks/useProducts";
+import "./ProductList.css";
 
-const ProductList = () => {
+function ProductList() {
+  const { products, loading, error } = useProducts();
+
+  if (loading) {
+    return <h2 style={{ textAlign: "center" }}>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2 style={{ color: "red", textAlign: "center" }}>{error}</h2>;
+  }
+
   return (
-    <div>ProductList</div>
-  )
+    <div className="products-grid">
+      {products.map((product) => (
+        <ProductItem
+          key={product.id}
+          product={product}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default ProductList
+export default ProductList;
