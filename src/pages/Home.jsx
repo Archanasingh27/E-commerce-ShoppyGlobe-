84806218d7ae1e380c3b8650c8c47c47770cd873
Archana <rathourarchna922@gmail.com>
2026-07-04 03:00:13 +0530

    Update Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header/Header.jsx';
+import ProductList from '../components/Product/ProductList.jsx';
+
+const Home = () => {
+  return (
+    <>
+          <Header />
+        <ProductList />
+    </>
+  );
+}
+
+export default Home
