cf198703b27058c3bae75ef14fae871bce11ac58
Archana <rathourarchna922@gmail.com>
2026-07-04 02:38:35 +0530

    implement custom hooks for fetching products

---
+++ src/hooks/useProduct.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import api from "../api/api";
+
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+
+        const response = await api.get("/products");
+
+        setProducts(response.data.products);
+      } catch (err) {
+        setError("Failed to fetch products.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+};
+
+export default useProducts;
