deebdbe00ac94cd0c0a5251d3d7e2332a13a37e3
Archana <rathourarchna922@gmail.com>
2026-07-04 02:21:39 +0530

    Update Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="logo">
+        <Link to="/">🛍️ ShoppyGlobe</Link>
+      </div>
+
+      <nav className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/cart" className="cart-link">
+          🛒 Cart
+          <span className="cart-count">0</span>
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header
