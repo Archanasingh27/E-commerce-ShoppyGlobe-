d78c9df4cbe8888167bd5d7a58e6ea47924ced52
Archana <rathourarchna922@gmail.com>
2026-07-04 02:51:54 +0530

    Create product card component

--- src/components/Product/ProductItem.jsx
+++ src/components/Product/ProductItem.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "./ProductItem.css";
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
   return (
-    <div>ProductItem</div>
-  )
+    <div className="product-card">
+      <img src={product.thumbnail} alt={product.title} loading="lazy" />
+
+      <h3>{product.title}</h3>
+
+      <p>${product.price}</p>
+
+      <Link to={`/product/${product.id}`}>
+        <button>View Details</button>
+      </Link>
+
+      <button>Add to Cart</button>
+    </div>
+  );
 }
 
 export default ProductItem
