bb762b23558c0bb6cb9f4561b3c1373c1b0a674f
Archana <rathourarchna922@gmail.com>
2026-07-04 18:02:46 +0530

    add herosection

---
+++ src/Herosection/Herosection.jsx
@@ -0,0 +1,42 @@
+import "./Herosection.css";
+import heroImg from "../assets/hero.png";
+
+function Hero() {
+  return (
+    <section className="hero">
+
+      <div className="hero-left">
+
+        <span className="hero-badge">
+          ✨ Discover the Latest Collections
+        </span>
+
+        <h1>
+          Elevate Your
+          <br />
+          Everyday
+          <br />
+          Essentials
+        </h1>
+
+        <p>
+          Welcome to ShoppyGlobe. Explore premium gadgets,
+          fashion, beauty products and accessories with
+          amazing deals and fast delivery.
+        </p>
+
+        <button className="hero-btn">
+          Explore Products
+        </button>
+
+      </div>
+
+      <div className="hero-right">
+        <img src={heroImg} alt="Hero" />
+      </div>
+
+    </section>
+  );
+}
+
+export default Hero;
