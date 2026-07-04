a456ea7a20581fc177d83c855dd26051014383d1
Archana <rathourarchna922@gmail.com>
2026-07-04 18:33:31 +0530

    add categoryBar

---
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -0,0 +1,34 @@
+import "./CategoryBar.css";
+
+function CategoryBar({ categories, selectedCategory, setSelectedCategory }) {
+  return (
+    <section className="category-section">
+      <div className="category-title">
+        <h2>Our Catalog</h2>
+
+        <span>{categories.length} Categories</span>
+      </div>
+
+      <div className="category-list">
+        <button
+          className={selectedCategory === "all" ? "active-category" : ""}
+          onClick={() => setSelectedCategory("all")}
+        >
+          All
+        </button>
+
+        {categories.map((category) => (
+          <button
+            key={category}
+            className={selectedCategory === category ? "active-category" : ""}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default CategoryBar;
